2e4f64e86d2676b87bb7c3e464738153c0a4a460
Leonard Monteiro <leonardommonteiro2004@gmail.com>
2024-10-21 20:28:53 -0300

    calsse de acessos p1

--- js/registrar.js
+++ js/registrar.js
@@ -1,3 +1,6 @@
+let users = JSON.parse(localStorage.getItem('users')) || [];
+users.push({ email: "campergames68@gmail.com", password: "1234", username: "ADM",access:"admin"});
+
 document.getElementById('registerForm').addEventListener('submit', function(event) {
     event.preventDefault(); // Impede o envio do formulário tradicional
   
@@ -6,15 +9,12 @@ document.getElementById('registerForm').addEventListener('submit', function(even
     const email = document.getElementById('email').value;
     const password = document.getElementById('password').value;
     const confirmPassword = document.getElementById('confirmPassword').value;
-  
+ 
     // Verifica se as senhas são iguais
     if (password !== confirmPassword) {
         
         return; // Para o processo aqui se as senhas não forem iguais
     }
-  
-    // Obtém os usuários cadastrados do localStorage ou cria um array vazio se não houver
-    let users = JSON.parse(localStorage.getItem('users')) || [];
   
     // Verifica se o email de usuário já existe
     const userExists = users.some(user => user.email === email);
@@ -24,7 +24,7 @@ document.getElementById('registerForm').addEventListener('submit', function(even
     }
   
     // Adiciona o novo usuário ao array
-    users.push({ email: email, password: password, username: username});
+    users.push({ email: email, password: password, username: username,access:"public"});
   
     // Armazena o array de usuários de volta no localStorage
     localStorage.setItem('users', JSON.stringify(users));
@@ -32,5 +32,4 @@ document.getElementById('registerForm').addEventListener('submit', function(even
    ;
     // Redireciona para a página de login
     window.location.href = 'login.html';
-  });
-  
+  });  
